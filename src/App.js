e8633a3afea90809099c96f38f3bc2779458789d
Sarah Thompson <sarah.thompson@raventelemetry.com>
2022-10-07 14:05:56 -0400

    format buttons, add scss to project, add prettier

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
 // import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 // import image1 from "./images/image1.png";
 
 function App() {
   return (
     <div className="App">
-      {/* <img src={image1} alt="meat"></img> */}
-      <div
-        className="App-landing"
-        // style={{
-        //   background: `url(${image1})`,
-        //   backgroundRepeat: "no-repeat",
-        //   backgroundSize: "cover",
-        //   backgroundColor: "rgba(0, 0, 0, 0.5)",
-        // }}
-      >
+      <div className="App-landing">
         <header>
           <p>Fresh, locally raised Prince Edward Island Meats.</p>
-          <button>View cuts</button>
-          <button>Gallery</button>
+          <div className="button-container">
+            <button className="primary">View cuts</button>
+            <button className="secondary">Gallery</button>
+          </div>
         </header>
       </div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
