7ffa275d7a9edf989f2dfb37766622b0bba9363a
Sarah Thompson <sethompson@upei.ca>
2022-10-09 15:40:51 -0300

    add middle section of landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-// import logo from "./logo.svg";
 import "./App.scss";
-// import image1 from "./images/image1.png";
 
 function App() {
+
+  const FACTS_LIST = [
+    "A proud producer of PEI Certified Beef", 
+    "Farm fresh custom cuts of beef, pork, and chicken", 
+    "Offering our customers quality meat cuts at affordable prices"
+  ];
+
   return (
     <div className="App">
-      <div className="AppLanding">
-        <div className="LandingContent">
+      <div id="AppLandingTop" className="AppLandingTop FullScreenBackground">
           <header>
             <h1>Fresh, locally raised Prince Edward Island Meats.</h1>
           </header>
@@ -14,11 +18,21 @@ function App() {
             <button className="Primary">View cuts</button>
             <button className="Secondary">Gallery</button>
           </div>
-          {/* TODO: fix absolute positioning of button but keep it at the bottom */}
           <div className="WhoAreWeButtonContainer">
             <button className="WhoAreWeButton Secondary">Who are we?</button>
           </div>
-        </div>
+      </div>
+      <div id="AppLandingMiddle" className="AppLandingMiddle FullScreenBackground">
+        <h2>A local family with a history of serving the people of Prince Edward Island.</h2>
+        <ul>
+          {FACTS_LIST.map((item, idx) => 
+            <li key={idx}>{item}</li>
+          )}
+        </ul>
+        <div className="ButtonContainer right">
+            <button className="Primary">About us</button>
+            <button className="Secondary">View cuts</button>
+          </div>
       </div>
     </div>
   );
